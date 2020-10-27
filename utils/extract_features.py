dca41a1ac04351873c56e57c16f29040382e2278
Tetiana Klymchuk <missklimchuck@yandex.ua>
2020-10-27 11:08:24 +0100

    [NEW] Added distance computiong function
    ]

--- utils/extract_features.py
+++ utils/extract_features.py
@@ -99,3 +99,25 @@ def compute_user_statistics(response):
 
     return row, time_stamp
 
+
+    def edit_distance(s1, s2):
+    """
+    Implementation of Levenshtein distance (https://en.wikipedia.org/wiki/Levenshtein_distance)
+    :param s1:
+    :param s2:
+    :return: levenshtein distance
+    """
+    if len(s1) > len(s2):
+        s1, s2 = s2, s1
+
+    distances = range(len(s1) + 1)
+    for i2, c2 in enumerate(s2):
+        distances_ = [i2 + 1]
+        for i1, c1 in enumerate(s1):
+            if c1 == c2:
+                distances_.append(distances[i1])
+            else:
+                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+        distances = distances_
+
+    return distances[-1]
